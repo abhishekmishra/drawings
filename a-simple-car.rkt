570f51090249f75fb358059dcccd6c9d46738c67
Abhishek Mishra <abhishekmishra3@yahoo.com>
2017-05-10 20:00:18 +0530

    added wheels to the car

--- a-simple-car.rkt
+++ a-simple-car.rkt
@@ -4,7 +4,7 @@
 (require racket/gui)
 (require (prefix-in greg: gregor))
 
-(define target (make-bitmap 500 500))
+(define target (make-bitmap 1500 500))
 (define dc (new bitmap-dc% [bitmap target]))
 
 ; See documentation about smoothing
@@ -13,16 +13,44 @@
 
 (send dc translate 100 100)
 
-(send dc draw-rectangle
-      0 0
-      200 100)
+;(send dc draw-rectangle
+;      0 0
+;      200 100)
+;
+;(send dc translate 0 -50)
+;
+;(send dc draw-arc
+;      0 0
+;      200 100
+;      0 3.14)
 
-(send dc translate 0 -50)
+(define (make-wheel wheel-size)
+  ;(define (make-an-x length)
+  ;  (let ((quarter (/ length 4)))
+  ;    (send dc draw-line quarter quarter length length)
+  ;    (send dc draw-line quarter length length quarter)))
 
-(send dc draw-arc
-      0 0
-      200 100
-      0 3.14)
+  (define (make-a-+ length)
+    (let ((half (/ length 2)))
+      (send dc draw-line 0 half length half)
+      (send dc draw-line half 0 half length)))
 
+
+  (send dc draw-ellipse
+        0 0
+        wheel-size wheel-size)
+
+  (send dc draw-ellipse
+        (- (/ wheel-size 2) 5) (- (/ wheel-size 2) 5)
+        10 10)
+
+  ;(make-an-x wheel-size)
+  (make-a-+ wheel-size))
+
+(make-wheel 100)
+
+(send dc translate 400 0)
+
+(make-wheel 100)
 (make-object image-snip% target)
 
